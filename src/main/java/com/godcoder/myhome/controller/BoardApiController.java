c1b68bca9a65c32969f5ea393b30d506a2a927c1
이효인 <hyoin@trycatch.kr>
2023-01-23 00:55:12 +0900

    form 전송, JPA rest api, JPA 페이징 추가

---
+++ src/main/java/com/godcoder/myhome/controller/BoardApiController.java
@@ -0,0 +1,62 @@
+package com.godcoder.myhome.controller;
+
+import com.godcoder.myhome.Repository.BoardRepository;
+import com.godcoder.myhome.model.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository repository;
+
+    @GetMapping("/board")
+    List<Board> all(
+            @RequestParam(required = false, defaultValue = "") String title,
+            @RequestParam(required = false, defaultValue = "") String content) {
+        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)){
+            return repository.findAll();
+        }else{
+//            return repository.findByTitle(title);
+            return repository.findByTitleOrContent(title, content);
+        }
+    }
+
+    @PostMapping("/board")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    // Single item
+
+    @GetMapping("/board/{id}")
+    Board one(@PathVariable Long id) {
+
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/board/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return repository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/board/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
