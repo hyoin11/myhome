022ae044fceb333fb761e16a0e2543cf751d2ee0
이효인 <hyoin@trycatch.kr>
2023-01-26 12:51:47 +0900

    쿼리문 수정

--- src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
@@ -40,13 +40,52 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(passwordEncoder())
-                .usersByUsernameQuery("select email,password,enabled "
-                        + "from bael_users "
-                        + "where email = ?")
-                .authoritiesByUsernameQuery("select email,authority "
+                .usersByUsernameQuery("select username,password,enabled "
+                        + "from user "
+                        + "where username = ?")
+                .authoritiesByUsernameQuery("select username,authority "
                         + "from authorities "
                         + "where email = ?");
     }
+    /*
+        Authentication  로그인
+        Authroization   권한
+
+        create table board(
+	id serial primary key,
+	title varchar(50),
+	content text
+);
+
+create table "user"(
+	id serial primary key,
+	username varchar(50) not null,
+	password varchar(100) not null,
+	enabled bit(1) not null
+)
+
+create table role(
+	id serial primary key,
+	name varchar(50) not null
+)
+
+create table user_role(
+	user_id bigint,
+	role_id bigint,
+	primary key(user_id, role_id),
+	foreign key (user_id) references "user"(id),
+	foreign key (role_id) references role(id)
+)
+
+@OneToOne
+	ex) user - user_detail
+@OneToMany
+	ex) user - board
+@ManyToOne
+	ex) board - user
+@ManyToMany
+	ex) user - role
+     */
 
     @Bean
     public PasswordEncoder passwordEncoder() {
