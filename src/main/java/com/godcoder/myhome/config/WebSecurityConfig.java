42b29ed173927ad1ad05120fde5c3b517f7c9404
hyoin11 <kslove1111@gmail.com>
2023-01-29 18:45:40 +0900

    User, Role model 추가, security 추가

--- src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
@@ -23,11 +23,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
+                    .antMatchers("/", "/account/register", "/css/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/login")
+                    .loginPage("/account/login")
                     .permitAll()
                     .and()
                 .logout()
@@ -41,50 +41,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .dataSource(dataSource)
                 .passwordEncoder(passwordEncoder())
                 .usersByUsernameQuery("select username,password,enabled "
-                        + "from user "
+                        + "from \"user\" "
                         + "where username = ?")
-                .authoritiesByUsernameQuery("select username,authority "
-                        + "from authorities "
-                        + "where email = ?");
+                .authoritiesByUsernameQuery("select u.username, r.name "
+                        + "from user_role ur inner join \"user\" u on ur.user_id = u.id "
+                        + "inner join role r on ur.role_id = r.id "
+                        + "where u.username = ?");
     }
     /*
         Authentication  로그인
         Authroization   권한
-
-        create table board(
-	id serial primary key,
-	title varchar(50),
-	content text
-);
-
-create table "user"(
-	id serial primary key,
-	username varchar(50) not null,
-	password varchar(100) not null,
-	enabled bit(1) not null
-)
-
-create table role(
-	id serial primary key,
-	name varchar(50) not null
-)
-
-create table user_role(
-	user_id bigint,
-	role_id bigint,
-	primary key(user_id, role_id),
-	foreign key (user_id) references "user"(id),
-	foreign key (role_id) references role(id)
-)
-
-@OneToOne
-	ex) user - user_detail
-@OneToMany
-	ex) user - board
-@ManyToOne
-	ex) board - user
-@ManyToMany
-	ex) user - role
      */
 
     @Bean
